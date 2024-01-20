d5980faaad0812b78fa112f6910badea9f6954cb
netolobo <desilio@gmail.com>
2024-01-20 09:31:47 -0800

    Initial Commit

---
+++ GalaxianSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GalaxianSwiftUI
+//
+//  Created by Neto Lobo on 20/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GalaxianSwiftUI/GalaxianSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxianSwiftUIApp.swift
+//  GalaxianSwiftUI
+//
+//  Created by Neto Lobo on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GalaxianSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
